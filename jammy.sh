d41521b699e66caed00bfe4c742757b8bda1cd45
Angga <angga@linuxmail.org>
2021-11-20 22:58:48 +0700

    jammy-jellyfish: initial commit
    Change-Id: Id3d6695440e8e892f3312713cc30d8fe6c4b32da
    Signed-off-by: Angga <angga@linuxmail.org>

---
+++ jammy.sh
@@ -0,0 +1,19 @@
+rm MD5SUMS
+rm MD5SUMS.gpg
+rm SHA1SUMS
+rm SHA1SUMS.gpg
+rm SHA256SUMS
+rm SHA256SUMS.gpg
+rm build-info.txt
+rm ubuntu-impish-core-cloudimg-amd64-root.tar.gz
+rm ubuntu-impish-core-cloudimg-amd64.manifest
+
+wget https://partner-images.canonical.com/core/jammy/current/MD5SUMS
+wget https://partner-images.canonical.com/core/jammy/current/MD5SUMS.gpg
+wget https://partner-images.canonical.com/core/jammy/current/SHA1SUMS
+wget https://partner-images.canonical.com/core/jammy/current/SHA1SUMS.gpg
+wget https://partner-images.canonical.com/core/jammy/current/SHA256SUMS
+wget https://partner-images.canonical.com/core/jammy/current/SHA256SUMS.gpg
+wget https://partner-images.canonical.com/core/jammy/current/ubuntu-impish-core-cloudimg-amd64-root.tar.gz
+wget https://partner-images.canonical.com/core/jammy/current/ubuntu-impish-core-cloudimg-amd64.manifest
+wget https://partner-images.canonical.com/core/jammy/current/unpacked/build-info.txt
